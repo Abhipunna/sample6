7938fb949fd918b07af9c12fca54b0d8068205ba
anu102 <anvithasheelam@gmail.com>
2024-12-10 15:01:53 +0530

    first commit

---
+++ prime.java
@@ -0,0 +1,20 @@
+
+public class Prime {
+    public static void main(String[] args) {
+        if (isPrime(num)) {
+            return true;
+        }
+    }
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
